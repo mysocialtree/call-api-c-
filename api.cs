450af8f9868eab54a85140763d776a769ba7aa5d
mstree <111582436+mysocialtree@users.noreply.github.com>
2022-08-19 20:09:38 +0200

    Update api.cs

--- api.cs
+++ api.cs
@@ -1,1 +1,20 @@
+Console.WriteLine("Satrting ...");
 
+// url https://mstree.de/api/get.php + ? + api=token
+string url = "https://mstree.de/api/get.php?api=token";
+
+// get data from Server
+System.Net.WebClient wc = new System.Net.WebClient();
+byte[] raw = wc.DownloadData(url);
+
+// encode data
+string webData = System.Text.Encoding.UTF8.GetString(raw);
+Console.WriteLine(webData);
+
+    //parse it
+    var yourObject = System.Text.Json.JsonDocument.Parse(webData);
+    //retrieve the value
+    var id = yourObject.RootElement
+                      .GetProperty("id");
+
+    Console.WriteLine(id);
